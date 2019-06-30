4267ec6e6b4eba66a02b1ce1b5a11055645d62ad
LaQuay <marcvilagomez@gmail.com>
2019-06-30 15:45:50 +0200

    improve about fragment

--- mobile/src/main/java/laquay/com/canalestdt/AboutFragment.java
+++ mobile/src/main/java/laquay/com/canalestdt/AboutFragment.java
@@ -1,5 +1,7 @@
 package laquay.com.canalestdt;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
     public static final String TAG = AboutFragment.class.getSimpleName();
     private View rootView;
+    private TextView appVersionTV;
 
     public static AboutFragment newInstance() {
         return new AboutFragment();
@@ -24,11 +28,23 @@ public class AboutFragment extends Fragment {
         setUpElements();
         setUpListeners();
 
+        setVersion();
+
         return rootView;
     }
 
-    private void setUpElements() {
+    private void setVersion() {
+        try {
+            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
 
+            appVersionTV.setText("v" + pInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUpElements() {
+        appVersionTV = rootView.findViewById(R.id.fragment_about_version_name_tv);
     }
 
     private void setUpListeners() {
